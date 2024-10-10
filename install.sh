7af9da0ace379671e56b1349d3818046cc180215
denyocr93 <flutterlabz@gmail.com>
2024-10-10 07:36:17 +0700

    .

--- install.sh
+++ install.sh
@@ -108,10 +108,10 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php | php && sudo p
 
 
 # SETUP LETSENCRYPT
-sudo apt install snapd -y
-sudo snap install core
-sudo snap refresh core
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot --apache --email "$WEB_EMAIL" --domain "$WEB_DOMAIN" --agree-tos --non-interactive
-echo "0 3 1 * * /usr/bin/certbot renew --quiet" | crontab -
+# sudo apt install snapd -y
+# sudo snap install core
+# sudo snap refresh core
+# sudo snap install --classic certbot
+# sudo ln -s /snap/bin/certbot /usr/bin/certbot
+# sudo certbot --apache --email "$WEB_EMAIL" --domain "$WEB_DOMAIN" --agree-tos --non-interactive
+# echo "0 3 1 * * /usr/bin/certbot renew --quiet" | crontab -
